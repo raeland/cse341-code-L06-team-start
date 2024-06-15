a51c1bb7bc852d8f023eef00c6753988839584db
raeland <rsvarady@gmail.com>
2024-06-14 21:50:49 -0400

    personal project...

--- swagger.js
+++ swagger.js
@@ -2,8 +2,8 @@ const swaggerAutogen = require('swagger-autogen')()
 
 const doc = {
   info: {
-    title: 'My API',
-    description: 'Temple API'
+    title: 'Supplies API',
+    description: 'Supplies API'
   },
   host: 'localhost:8080',
   schemes: ['http','https']
